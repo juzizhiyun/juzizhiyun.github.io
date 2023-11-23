fcb16f86520534788d75e151c6fbcff51f061b2d
juzi <89078935+juzizhiyun@users.noreply.github.com>
2023-11-23 16:24:35 +0800

    Add files via upload

--- jsevent.js
+++ jsevent.js
@@ -1,4 +1,10 @@
 function onclick_func() {
-  alert("触发点击事件");
-  document.write("successful");
+    alert("触发点击事件");
+    document.write("successful");
+}
+
+function changeself() {
+    var changewill = document.getElementById("js_change");
+    var code = changewill.innerHTML;
+    alert(code);
 }
